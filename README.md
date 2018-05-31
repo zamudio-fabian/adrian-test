ff1a1a76857cae6cdee2a156bd263f07f62fe700
fabian <fabian.zamudio.89@gmail.com>
2018-05-31 12:17:41 -0300

    First test

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # adrian-test
 Primera entrega para trabajo en Workana
+
+
+Consignas Test
+
+Desarrollo Full-Stack
+
+A partir del boceto, construye en código PHP un index que utilice el archivo db.json para
+construir un HTML basado en los bocetos planteados en la carpeta MATERIAL.
+
+HTML ➔ Usar etiquetas de forma semántica. ➔ Usar clases legibles para otros programadores,
+podes usar metodologías como https://en.bem.info/methodology/
+
+SASS ➔ Usar un pre​procesador (ej: Sass) y post​procesador (ej: Autoprefixer). ➔ Minificar el
+CSS (en el proceso de build, con Gulp, Grunt o Prepros). ➔ Escribir el CSS en varios archivos,
+pero usar en el HTML un solo CSS generado.
+
+Material ➔ Para el material iconográfico utilizar ​http://fontawesome.io/icons/​. ➔ No debe
+contener ninguna imagen.
---
+++ index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Adrian test</title>
+  </head>
+  <body>
+    <nav class="navbar">
+    	<div class="container">
+            <div class="row w-100">
+                <div class="col-12 col-md-4">
+                    <h5>Soy Front-end</h5>
+                </div>
+                <div class="col-12 col-md-8 text-right  d-none d-md-block">
+                    <a href="#link1" class="nav-link" >link1</a>
+                    <a href="#link2" class="nav-link active">link2</a>
+                    <a href="#link3" class="nav-link" >link3</a>
+                </div>
+            </div>
+    	</div>
+    </nav>
+    <div class=" d-md-none py-1">
+        <a href="#link1" class="nav-link" >link1</a>
+        <a href="#link2" class="nav-link active">link2</a>
+        <a href="#link3" class="nav-link" >link3</a>
+    </div>
+    <main class="container my-3">
+        <div class="row top-section px-4 pt-4">
+            <div class="col-12 col-md-8 py-5">
+                <h4 class="text-capitalize">Lorem ipsum dolar</h4>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div class="text-center">
+                    <button type="button" class="btn btn-secondary btn-lg btn-download">DESCARGAR</button>
+                </div>
+            </div>
+            <div class="col-12 col-md-4 d-none d-md-block">
+                <div class="sidebar-right">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row py-5 mt-3">
+            <div class="col-12 col-md-4">
+                <h4 class="text-capitalize">Aurora</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h4 class="text-capitalize">moment</h4>
+                <ul class="padding-to-left">
+                    <li>At vero eos et accusamus et iusto odio</li>
+                    <li>At vero eos et accusamus et iusto odio</li>
+                    <li>At vero eos et accusamus et iusto odio</li>
+                    <li>At vero eos et accusamus et iusto odio</li>
+                    <li>At vero eos et accusamus et iusto odio</li>
+                    <li>At vero eos et accusamus et iusto odio</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h4 class="text-capitalize">Screenshots</h4>
+                <div class="container-fluid">
+                    <div id="carouselExample" class="carousel slide" data-ride="carousel" data-interval="9000">
+                        <div class="carousel-inner row w-100 mx-auto" role="listbox">
+                            <div class="carousel-item col-3 active">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 1">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 2">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 3">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 4">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 5">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 6">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                            <div class="carousel-item col-3">
+                                <img class="img-fluid mx-auto d-block" src="assets/images/img.jpg" alt="slide 7">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                            <i class="fa fa-chevron-left fa-lg"></i>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next text-faded" href="#carouselExample" role="button" data-slide="next">
+                            <i class="fa fa-chevron-right fa-lg"></i>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="text-center py-5">
+        <h5 class="my-5">Gracias por tomarte este tiempo :)</h5>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script>
+        $('#carouselExample').on('slide.bs.carousel', function (e) {
+
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $('.carousel-item').length;
+        
+        if (idx >= totalItems-(itemsPerSlide-1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i=0; i<it; i++) {
+                // append slides to end
+                if (e.direction=="left") {
+                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                }
+                else {
+                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                }
+            }
+        }
+    });
+        
+    </script>
+  </body>
+</html>
